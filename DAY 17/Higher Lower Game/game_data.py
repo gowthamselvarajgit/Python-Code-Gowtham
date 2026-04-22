0ee482d431492d5b8f9b76636f54bd73d441888b
gowthamselvarajgit <gowtham26.work@gmail.com>
2026-04-22 13:43:22 +0530

    Completed Higher Lower Game Project

---
+++ DAY 17/Higher Lower Game/game_data.py
@@ -0,0 +1,146 @@
+data = [
+    {
+        'name': 'A. R. Rahman',
+        'follower_count': 14,
+        'description': 'Music composer and Oscar winner',
+        'country': 'India'
+    },
+    {
+        'name': 'Suriya',
+        'follower_count': 9,
+        'description': 'Tamil cinema actor and producer',
+        'country': 'India'
+    },
+    {
+        'name': 'Ajith Kumar',
+        'follower_count': 13,
+        'description': 'Tamil cinema actor and racer',
+        'country': 'India'
+    },
+    {
+        'name': 'Sun Pictures',
+        'follower_count': 5,
+        'description': 'Film production company',
+        'country': 'India'
+    },
+    {
+        'name': 'Kamal Haasan',
+        'follower_count': 10,
+        'description': 'Actor, filmmaker, and politician',
+        'country': 'India'
+    },
+    {
+        'name': 'BCCI',
+        'follower_count': 25,
+        'description': 'Governing body for cricket in India',
+        'country': 'India'
+    },
+    {
+        'name': 'KL Rahul',
+        'follower_count': 22,
+        'description': 'Indian cricketer',
+        'country': 'India'
+    },
+    {
+        'name': 'Dhanush',
+        'follower_count': 8,
+        'description': 'Actor, musician, and filmmaker',
+        'country': 'India'
+    },
+    {
+        'name': 'Anirudh Ravichander',
+        'follower_count': 11,
+        'description': 'Music composer and singer',
+        'country': 'India'
+    },
+    {
+        'name': 'Vikram',
+        'follower_count': 7,
+        'description': 'Tamil cinema actor',
+        'country': 'India'
+    },
+    {
+        'name': 'Rohit Sharma',
+        'follower_count': 35,
+        'description': 'Indian cricketer',
+        'country': 'India'
+    },
+    {
+        'name': 'Chennai Super Kings',
+        'follower_count': 35,
+        'description': 'IPL cricket franchise',
+        'country': 'India'
+    },
+    {
+        'name': 'Vijay',
+        'follower_count': 18,
+        'description': 'Tamil cinema actor and singer',
+        'country': 'India'
+    },
+    {
+        'name': 'Lyca Productions',
+        'follower_count': 4,
+        'description': 'Film production company',
+        'country': 'India'
+    },
+    {
+        'name': 'Mumbai Indians',
+        'follower_count': 32,
+        'description': 'IPL cricket franchise',
+        'country': 'India'
+    },
+    {
+        'name': 'Trisha',
+        'follower_count': 6,
+        'description': 'Actress',
+        'country': 'India'
+    },
+    {
+        'name': 'Jasprit Bumrah',
+        'follower_count': 18,
+        'description': 'Indian fast bowler',
+        'country': 'India'
+    },
+    {
+        'name': 'Hardik Pandya',
+        'follower_count': 30,
+        'description': 'Indian all-rounder',
+        'country': 'India'
+    },
+    {
+        'name': 'Virat Kohli',
+        'follower_count': 260,
+        'description': 'Indian cricketer',
+        'country': 'India'
+    },
+    {
+        'name': 'Indian Premier League',
+        'follower_count': 40,
+        'description': 'Professional T20 cricket league',
+        'country': 'India'
+    },
+    {
+        'name': 'Rajinikanth',
+        'follower_count': 15,
+        'description': 'Tamil cinema actor and cultural icon',
+        'country': 'India'
+    },
+    {
+        'name': 'Nayanthara',
+        'follower_count': 12,
+        'description': 'Tamil cinema actress',
+        'country': 'India'
+    },
+    {
+        'name': 'MS Dhoni',
+        'follower_count': 45,
+        'description': 'Former Indian cricket captain',
+        'country': 'India'
+    },
+    {
+        'name': 'Sachin Tendulkar',
+        'follower_count': 50,
+        'description': 'Former Indian cricketer',
+        'country': 'India'
+    }
+]
---
+++ DAY 17/Higher Lower Game/main.py
@@ -0,0 +1,49 @@
+import random
+from game_data import data
+from game_art import logo, vs  
+
+random_a = random.choice(data)
+def get_random_accounts(game_data):
+    global random_a
+    random_b = random.choice(game_data)
+
+    while random_a == random_b:
+        random_b = random.choice(game_data)
+
+    print(f"Compare A: {random_a['name']}, a {random_a['description']}, from {random_a['country']}.")
+    print(vs)
+    print(f"Against B: {random_b['name']}, a {random_b['description']}, from {random_b['country']}.")
+    return random_a, random_b
+
+def followers_compare(account_a,account_b):  
+    global random_a
+    input_guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    if account_a['follower_count'] > account_b['follower_count']:
+        correct_answer = 'a'
+        random_a = account_a
+    else:
+        correct_answer = 'b'
+        random_a = account_b
+
+    if input_guess == correct_answer:
+        print(random_a)
+        return True
+    else:
+        return False
+
+game_should_continue = True
+score = 0
+
+while game_should_continue:
+    print(logo)
+    print(f"Current score: {score}.")
+    random_accounts = get_random_accounts(data)
+    result = followers_compare(random_accounts[0], random_accounts[1])
+
+    if result == True:
+        score += 1
+        print(f"You're right! Current score: {score}.")
+        print("\n" * 20)
+    else:
+        print(f"Sorry, that's wrong. Final score: {score}.")
+        game_should_continue = False
